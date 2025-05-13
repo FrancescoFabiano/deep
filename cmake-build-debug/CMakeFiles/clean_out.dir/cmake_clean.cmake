8c845aef0e87e2b7dd833056588d599ec296855d
FrancescoFabiano <francesco.fabi94@gmail.com>
2025-05-13 12:11:47 +0100

    Added cmake that works also with clion

--- cmake-build-debug/CMakeFiles/clean_out.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean_out"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean_out.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/prepare_build_dir.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/prepare_build_dir"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/prepare_build_dir.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
